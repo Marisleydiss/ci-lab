625ac6e79a4799183f84821d698409fbfbabf472
Marisleydiss <88462899+Marisleydiss@users.noreply.github.com>
2023-10-30 20:58:18 -0300

    Update App.java
    2do public

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,9 +6,16 @@ package com.mycompany.app;
 public class App {
   
 
-  public static String triangulo(int a, int b, int c) {
+ public static String triangulo(int a, int b, int c) {
+    if (a + b <= c || b + c <= a || c + a <= b) {
+      return "error no es un triangulo";
+    }
+    if (a == b && b == c && c == a)
+    {
+      return "es un triangulo equilatero";
+    }
     return "error no es un triangulo";
-  } 
+  }
   
 
   public static void main(String[] args) {
