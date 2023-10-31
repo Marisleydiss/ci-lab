9e3d3ec04c634a104636a2326d81f8f2f892d9f6
Marisleydiss <88462899+Marisleydiss@users.noreply.github.com>
2023-10-30 21:13:28 -0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,12 +7,28 @@ public class App {
   
 
 public static String triangulo(int a, int b, int c) {
-    if (a + b <= c || b + c <= a || c + a <= b) {
-      return "error no es un triangulo";
-    }
-    if (a == b && b == c && c == a)
-    {
-      return "es un triangulo equilatero";
+    if (a > 0 && b > 0 && c > 0) {
+ 
+      if (a * a + b * b == c * c || b * b + c * c == a * a || c * c + a * a == b * b) {
+        return "es un triangulo pitagorico";
+      }
+ 
+      if (a != b && b != c && c != a) {
+ 
+        return "es un triangulo escaleno";
+      }
+ 
+      if (a == b && b != c && c != a) {
+ 
+        return "es un triangulo isoceles";
+      }
+ 
+      if (a + b <= c || b + c <= a || c + a <= b) {
+        return "error no es un triangulo";
+      }
+      if (a == b && b == c && c == a) {
+        return "es un triangulo equilatero";
+      }
     }
     return "error no es un triangulo";
   }
