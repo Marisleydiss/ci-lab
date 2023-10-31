ffc0bdf00c5f274106ae78b3ba6360cd43bb32fa
Marisleydiss <88462899+Marisleydiss@users.noreply.github.com>
2023-10-30 21:02:23 -0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,6 +7,11 @@ public class App {
   
 
  public static String triangulo(int a, int b, int c) {
+    if(a == b && b != c && c != a){
+     
+      return "es un triangulo isoceles";
+    }
+ 
     if (a + b <= c || b + c <= a || c + a <= b) {
       return "error no es un triangulo";
     }
@@ -16,7 +21,6 @@ public class App {
     }
     return "error no es un triangulo";
   }
-  
 
   public static void main(String[] args) {
     System.out.println("Hola Gente!");
